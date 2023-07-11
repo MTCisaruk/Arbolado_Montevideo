1cad6e92d6a67b4e51268b2292e38f5c0cd3d3b1
MTCisaruk <manuel.toledo.cisaruk@gmail.com>
2023-07-11 20:06:00 -0300

    Presentacion

--- app/app.R
+++ app/app.R
@@ -4,14 +4,14 @@ library(sf)
 library(leaflet)
 library(terra)
 library(RColorBrewer)
-#library(xgboost)
-#library(caret)
-#library(tidymodels)
-#library(modeltime)
-#library(timetk)
+library(xgboost)
+library(caret)
+library(tidymodels)
+library(modeltime)
+library(timetk)
 library(shiny)
-#library(gt)
-#library(plotly)
+library(gt)
+library(plotly)
 
 precios <- readRDS("precios_canasta.RDS")
 
@@ -19,9 +19,9 @@ establecimientos <- read_delim("establecimiento.csv", delim = ";", locale = loca
   select(id.establecimientos, nombre.sucursal, ccz, cadena, long, lat, depto, id.depto) %>% 
   mutate(lat=as.numeric(sub(',','.',lat,fixed=T)),
          long=as.numeric(sub(',','.',long,fixed=T)),
-         # arreglamos Ta Ta que le faltaba el signo 
+         arreglamos Ta Ta que le faltaba el signo
          long = ifelse(long > 0, -34.88675, long),
-         # arreglamos San Roque del aeropuerto, que estaba mal
+         arreglamos San Roque del aeropuerto, que estaba mal
          long = ifelse(id.establecimientos == 679, -34.83675, long),
          lat = ifelse(id.establecimientos == 679, -56.01600, lat)) %>% 
   filter_at(c("long", "lat"), function(x) !is.na(x)) |> mutate(lng=lat,lat=long) |> select(-long)
@@ -113,44 +113,44 @@ server <- function(input, output) {
   
   
 
-  # output$modelo <- renderPlotly({
-  #   prod <- precios %>% 
-  #       filter(producto %in% input$eleg_producto & marca %in% input$eleg_marca) %>% 
-  #       group_by(year_month) %>% 
-  #       summarise(precio = mean(avg))
-  #   
-  #   if(nrow(prod)>0){
-  #   
-  #   splits <- time_series_split(prod, year_month, assess = "3 months")
-  #   
-  #   model_arima <- arima_reg() %>% 
-  #     set_engine("auto_arima") %>% 
-  #     fit(precio ~ year_month, training(splits))
-  #   
-  #    model_glmnet <- linear_reg(penalty = 0.01) %>% 
-  #      set_engine("glmnet") %>% 
-  #      fit(precio ~ month(year_month) + as.numeric(year_month), training(splits))
-  #   
-  #    model_prophet <- prophet_reg() %>% 
-  #      set_engine("prophet") %>% 
-  #      fit(precio ~ year_month, training(splits))
-  #   
-  #   model_tbl <- modeltime_table(
-  #      model_prophet,
-  #      model_glmnet,
-  #     model_arima)
-  #   
-  #   calib_tbl <- model_tbl %>% 
-  #     modeltime_calibrate(testing(splits))
-  #   future_forecast <- calib_tbl %>% 
-  #     modeltime_refit(prod) %>%
-  #     modeltime_forecast(h = "9 months", actual_data = prod)
-  #   
-  #   future_forecast %>% plot_modeltime_forecast(
-  #     .title = paste("Precio de", input$eleg_producto, "entre 2016 y 2023 ($)"),
-  #     .conf_interval_alpha = 0.1)
-  #   
-  #   }else{plotly_empty()}
-  # })
+  output$modelo <- renderPlotly({
+    prod <- precios %>%
+        filter(producto %in% input$eleg_producto & marca %in% input$eleg_marca) %>%
+        group_by(year_month) %>%
+        summarise(precio = mean(avg))
+
+    if(nrow(prod)>0){
+
+    splits <- time_series_split(prod, year_month, assess = "3 months")
+
+    model_arima <- arima_reg() %>%
+      set_engine("auto_arima") %>%
+      fit(precio ~ year_month, training(splits))
+
+     model_glmnet <- linear_reg(penalty = 0.01) %>%
+       set_engine("glmnet") %>%
+       fit(precio ~ month(year_month) + as.numeric(year_month), training(splits))
+
+     model_prophet <- prophet_reg() %>%
+       set_engine("prophet") %>%
+       fit(precio ~ year_month, training(splits))
+
+    model_tbl <- modeltime_table(
+       model_prophet,
+       model_glmnet,
+      model_arima)
+
+    calib_tbl <- model_tbl %>%
+      modeltime_calibrate(testing(splits))
+    future_forecast <- calib_tbl %>%
+      modeltime_refit(prod) %>%
+      modeltime_forecast(h = "9 months", actual_data = prod)
+
+    future_forecast %>% plot_modeltime_forecast(
+      .title = paste("Precio de", input$eleg_producto, "entre 2016 y 2023 ($)"),
+      .conf_interval_alpha = 0.1)
+
+    }else{plotly_empty()}
+  })
 }
 shinyApp(ui, server)
